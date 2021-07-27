0ad49cf31ef38cc0a05fac7b4f1300d8f9c48450
Alex <ovskykrolik@gmail.com>
2021-07-27 23:58:38 +0300

    cleanUp code

--- src/main/java/com/ivr/cafeback/controllers/MainController.java
+++ src/main/java/com/ivr/cafeback/controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
     @GetMapping("/")
-    public String simpleShow(){
+    public String simpleShow() {
         return "Hello this page updated";
     }
 }
